dd0cd664b9479510823310986fd2ca380f011b4f
TatchNicolas <TatchNicolas@users.noreply.github.com>
2021-06-08 11:12:50 +0900

    wip

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # slack-scaffold-python
+
+## Setup
+
+```
+$ export SLACK_APP_TOKEN=xapp-...
+$ export SLACK_BOT_TOKEN=xoxb-...
+$ poetry install --dev
+$ poetry run python app/main.py
+# live reload with watchdog
+$ watchmedo shell-command  --patterns="*.py" --recursive --command="python app/main.py
+```
--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 import os
+
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
+
+# Make sure app subscribes events you want to catch in Event Subscription page of your app setting
+@app.event("app_mention")
+def print_mention_event(event):
+    print(event)
+
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
